1b48266c80e0677042e4991ea275e05efdc5ad2b
rahulkrbxr <61363513+rahulkrbxr@users.noreply.github.com>
2023-10-03 13:19:01 +0000

    dynamic pages for each job

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
   jobs = load_jobs_from_db()
-  return render_template('home.html', jobs=jobs, company_name='Nova')
+  return render_template('home.html', jobs=jobs)
 
 
 @app.route("/api/jobs")
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  # return jsonify(job)
+  
+  if not job:
+    return "Not Found", 404
+  
+  return render_template('jobpage.html', job=job)
 
 
 print(__name__)
--- database.py
+++ database.py
@@ -45,3 +45,20 @@ def load_jobs_from_db():
     jobs.append(row_dict)
 
   return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("select * from jobs where id = :val"), 
+      dict(val=id))
+    
+  rows = result.all()
+  if len(rows) == 0:
+    return None
+  else:
+    column_names = result.keys()
+    row_dict = {}
+    for col, val in zip(column_names, rows[0]):
+      row_dict[col] = val
+    return row_dict
