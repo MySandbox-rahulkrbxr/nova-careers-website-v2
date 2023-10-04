c5dfbb782e6acb10b251d58c24831e0ea8b84903
rahulkrbxr <61363513+rahulkrbxr@users.noreply.github.com>
2023-10-04 06:10:57 +0000

    added an api

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
   jobs = load_jobs_from_db()
@@ -19,27 +20,34 @@ def list_jobs():
 def show_job(id):
   job = load_job_from_db(id)
   # return jsonify(job)
-  
+
   if not job:
     return "Not Found", 404
-  
+
   return render_template('jobpage.html', job=job)
+
+
+@app.route("/api/job/<id>")
+def show_job_json(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
 
 
 @app.route("/job/<id>/apply", methods=['post'])
 def apply_to_job(id):
   # data = request.args
   data = request.form
-  
+
   job = load_job_from_db(id)
   add_application_to_db(id, data)
-  
+
   # store this in the DB
   # send an email
   # display an acknowledgement
-  return render_template("application_submitted.html", 
+  return render_template("application_submitted.html",
                          application=data,
-                        job=job)
+                         job=job)
+
 
 print(__name__)
 if __name__ == "__main__":
